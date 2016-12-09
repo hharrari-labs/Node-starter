2bb0479eba30d098f74b79abcb5b6a7ffe29bdbe
Hakim <kim.k866@gmail.com>
2016-12-09 08:48:48 +0100

    create structure and add gitignore

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require("express"),
 	cookieParser = require("cookie-parser"),
 	bodyParser = require("body-parser");
+	mongoose = require('mongoose');
+ 
 
 var app = express();
 
@@ -9,6 +11,14 @@ var app = express();
 	app.use(bodyParser.json());
 	app.use(express.static(__dirname));
 	app.listen(8080);
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+	db.once('open', function() {
+	  // we're connected!
+});
+
 
 app.post("/",function(req,res){
 	res.sendFile(__dirname + '/index.html');
